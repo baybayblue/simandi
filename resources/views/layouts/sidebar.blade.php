17c9b33de8d45c2bc2afe786b128431171fb65ec
baybayblue <arindaayulestari.31@gmail.com>
2025-08-19 13:21:58 +0700

    templating

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,100 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.html" class="brand-link">
+        <img src="dist/img/simandi-logo.png" alt="Logo Simandi" class="brand-image img-circle elevation-3"
+            style="opacity: .9; background-color: white; padding: 3px;">
+        <span class="brand-text font-weight-bold">Simandi</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Alexander Pierce</a>
+            </div>
+        </div>
+
+        <!-- SidebarSearch Form -->
+        <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                    aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+                <!-- Dashboard -->
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+
+                <!-- Pelanggan -->
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-user"></i>
+                        <p>Pelanggan</p>
+                    </a>
+                </li>
+
+                <!-- Layanan -->
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-concierge-bell"></i>
+                        <p>Layanan</p>
+                    </a>
+                </li>
+
+                <!-- Transaksi -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-cash-register"></i>
+                        <p>
+                            Transaksi
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="pages/UI/ribbons.html" class="nav-link">
+                                <i class="far fa-plus-square nav-icon"></i>
+                                <p>Transaksi Baru</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/UI/ribbons.html" class="nav-link">
+                                <i class="far fa-list-alt nav-icon"></i>
+                                <p>Log Transaksi</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- Laporan -->
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-file-alt"></i>
+                        <p>Laporan</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
